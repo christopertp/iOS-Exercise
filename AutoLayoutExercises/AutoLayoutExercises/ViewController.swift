0916aa85698fa28bee4cd86aa16b7145020a5146
Christoper <christoper.t.p@gmail.com>
2020-05-25 15:45:50 +0700

    Add files via upload

---
+++ AutoLayoutExercises/AutoLayoutExercises/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutExercises
+//
+//  Created by Christoper on 16/05/20.
+//  Copyright © 2020 Christoper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
