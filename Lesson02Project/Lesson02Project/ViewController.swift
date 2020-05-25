2aa299e58b56cc329e14e6f71daaa18b72fbca09
Christoper <christoper.t.p@gmail.com>
2020-05-25 15:42:23 +0700

    Add files via upload

---
+++ Lesson02Project/Lesson02Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lesson02Project
+//
+//  Created by Christoper on 15/05/20.
+//  Copyright © 2020 Christoper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
